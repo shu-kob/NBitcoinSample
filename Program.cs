ea0b2abd18b3c0e6c7da8af2a99061c873d04178
Shu Kobuchi <shukobuchi+github@gmail.com>
2017-08-27 22:57:48 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using NBitcoin;
 using NBitcoin.DataEncoders;
 using NBitcoin.OpenAsset;
@@ -7,47 +7,47 @@ using QBitNinja.Client.Models;
 
 namespace NBitcoinFirst
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-		var coin = new Coin(
-			fromTxHash: new uint256("c8271ba7118308d6b3205475048d103887fe797b63393ebc1f7910abb9907d29"),
-			fromOutputIndex: 1,
-			amount: Money.Satoshis(12998727),
-			scriptPubKey: new Script(Encoders.Hex.DecodeData("76a914879d64086263c36d8bc1739471786b440e4e8b7488ac"))
-		);
-
-		var issuance = new IssuanceCoin(coin);
-
-		var nico = BitcoinAddress.Create("mst24ennQABZLfgr5HJC3GgzBbn5v4NinL");
-		var bookKey = new BitcoinSecret("cU8oQX6oazd9RFZ8t4EppdQX8BMj6icfCEG6CfVLXnMPFGTQmPZi");
-		TransactionBuilder builder = new TransactionBuilder();
-
-		var tx = builder
-			.AddKeys(bookKey)
-			.AddCoins(issuance)
-			.IssueAsset(nico, new AssetMoney(issuance.AssetId, quantity: 25000000))
-			.SendFees(Money.Coins(0.0001m))
-			.SetChange(bookKey.GetAddress())
-			.BuildTransaction(true);
-
-		Console.WriteLine(tx);
-                Console.WriteLine(builder.Verify(tx));
-
-		var client = new QBitNinjaClient(Network.TestNet);
-		BroadcastResponse broadcastResponse = client.Broadcast(tx).Result;
-
-		if (!broadcastResponse.Success)
+	class Program
+	{
+		static void Main(string[] args)
 		{
-			Console.WriteLine("ErrorCode: " + broadcastResponse.Error.ErrorCode);
-			Console.WriteLine("Error message: " + broadcastResponse.Error.Reason);
-		}
-		else
-		{
-			Console.WriteLine("Success!");
-		}
+			var coin = new Coin(
+				fromTxHash: new uint256("e2b308ea38112ae79ff2cc67d686c3dcd737214290802c1905b76e76c89783d4"),
+				fromOutputIndex: 0,
+				amount: Money.Satoshis(300000000),
+				scriptPubKey: new Script(Encoders.Hex.DecodeData("76a9141c770f57182260756319ca5e0af31e19b37288b288ac"))
+			);
 
+			var issuance = new IssuanceCoin(coin);
+
+			var nico = BitcoinAddress.Create("mwU3UJ1VXX3GxKKQHdscw1TvWSrMKdtymR");
+			var bookKey = new BitcoinSecret("cRETrCgfbU273XmpFDQr4GhsqU4cXB7ECQuoGtCrRmgy1U2jLu1f");
+			TransactionBuilder builder = new TransactionBuilder();
+
+			var tx = builder
+				.AddKeys(bookKey)
+				.AddCoins(issuance)
+				.IssueAsset(nico, new AssetMoney(issuance.AssetId, quantity: 21000000))
+				.SendFees(Money.Coins(0.001m))
+				.SetChange(bookKey.GetAddress())
+				.BuildTransaction(true);
+
+			Console.WriteLine(tx);
+			Console.WriteLine(builder.Verify(tx));
+
+			var client = new QBitNinjaClient(Network.TestNet);
+			BroadcastResponse broadcastResponse = client.Broadcast(tx).Result;
+
+			if (!broadcastResponse.Success)
+			{
+				Console.WriteLine("ErrorCode: " + broadcastResponse.Error.ErrorCode);
+				Console.WriteLine("Error message: " + broadcastResponse.Error.Reason);
+			}
+			else
+			{
+				Console.WriteLine("Success!");
+			}
+
+		}
 	}
-    }
 }
